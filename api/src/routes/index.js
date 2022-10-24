796eeee24be5f854f8fdb063160bcbb2424514fc
Ariel Monti <djaroldito@gmail.com>
2022-10-24 20:10:49 -0300

    new  2 routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,8 +4,10 @@ const router = Router();
 
 const allGuitars = require('./allGuitars.js')
 const allTtypes = require('./allTypes.js')
+const allBrands = require('./allBrands.js')
 
 router.use('/allguitars', allGuitars)
 router.use('/allTypes', allTtypes)
+router.use('/allBrands', allBrands)
 
 module.exports = router;
