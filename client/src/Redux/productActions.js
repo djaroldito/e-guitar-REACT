c97f6c8a6858d61442c6d7a3d972cb910792a56d
miguel-mm89 <miguelm.developer@outlook.com>
2022-10-27 20:33:01 -0300

    tod OK

--- client/src/Redux/productActions.js
+++ client/src/Redux/productActions.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-<<<<<<< HEAD
 import {
   getAllProducts,
   getProductById,
@@ -45,47 +44,3 @@ export const getByFilter = ({color, brand, type}) => (dispatch) => {
     .then((res) => dispatch(getByFilters(res.data)))
     .catch((err) => console.log(err));
 };
-=======
-import { getAllProducts, 
-        getProductById, 
-        getProductByBrand,
-        getProductFiltered } from "./productSlice";
-
-export const getAllPrds = () => (dispatch) => { //-----------------------------------------------------------
-    axios("http://localhost:3001/rguitars")
-    .then(res => dispatch(getAllProducts(res.data)))
-    .catch( error => console.log(error))
-}
-
-export const getById = (id) => (dispatch) => { //------------------------------------------------------------
-    axios(`http://localhost:3001/rguitars/${id}`)
-    .then(res => dispatch(getProductById(res.data)))
-    .catch(error => console.log(error))
-};
-
-export const getPrdBrand = (brand) => (dispatch) => { //-----------------------------------------------------
-    axios(`http://localhost:3001/rguitars?brand=${brand}`)
-    .then(res => dispatch(getProductByBrand(res.data)))
-    .catch(error => console.log(error))
-};
-
-export const getPrdFiltered = (filter) => (dispatch) => { //-------------------------------------------------
-    let { brand, type, color } = filter;
-
-    if (type === undefined && color === undefined) {
-        axios(`http://localhost:3001/rguitars?brand=${brand}`)
-        .then(res => dispatch(getProductFiltered(res.data)))
-        .catch(error => console.log(error))
-
-    } else if (color === undefined) {
-        axios(`http://localhost:3001/rguitars?brand=${brand}&type=${type}`)
-        .then(res => dispatch(getProductFiltered(res.data)))
-        .catch(error => console.log(error))
-
-    } else {
-        axios(`http://localhost:3001/rguitars?brand=${brand}&type=${type}&color=${color}`)
-        .then(res =>dispatch(getProductFiltered(res.data)))
-        .catch(error => console.log(error))
-    }
-}
->>>>>>> dev
