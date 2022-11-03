9edf64b5d21b13de3fcfa12730737675c59d2943
miguel-mm89 <miguelm.developer@outlook.com>
2022-11-03 19:47:22 -0300

    todo funcionando

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import store from "./Redux/store";
 import { Auth0Provider } from "@auth0/auth0-react";
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.REACT_APP_API || 'localhost:3001'
+axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001/'
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
