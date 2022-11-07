6526061783bed8e0218f8e52d58057765e6f6eaa
miguel-mm89 <miguelm.developer@outlook.com>
2022-11-07 15:54:04 -0300

    true

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const { conn } = require('./src/db.js');
 
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
     server.listen(process.env.PORT, function () {
     console.log('escuchando en puerto', process.env.PORT); // eslint-disable-line no-console
   });
