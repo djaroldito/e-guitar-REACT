88d72fdebe3bf9990f3835500e9bc10c7c3ff507
Ailín <ailin.tobares@gmail.com>
2022-11-10 12:35:05 -0300

    Ailin cambios menores

--- client/src/Redux/SignupActions.js
+++ client/src/Redux/SignupActions.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { getUser } from "./SignupSlice";
 
-<<<<<<< HEAD
 export const postSignupForm = /* async */ (supData) => {
     try {
         console.log("Estamos en axios")
@@ -9,23 +8,12 @@ export const postSignupForm = /* async */ (supData) => {
         .then(res => console.log("postUser Action response: ", res)) 
         console.log("Salimos del axios")
         return true
-=======
-export const postSignupForm = (supData) => {
-    try {
-        axios.post("/ruser/register", supData)
-        .then( res => console.log(res))
-
->>>>>>> dev
     } catch (error) {
         console.log("Esto tira userCreated:", error.message)
     }
 };
 
 export const getUserDB = (email) => (dispatch) => {
-<<<<<<< HEAD
-=======
-
->>>>>>> dev
     axios(`/ruser/email?email=${email}`)
     .then(res => dispatch(getUser(res.data)))
     .catch(error => console.log(error));
